97cc4a20ee3965c6df1d324fa79f6ee66925d0a8
Casmir Patterson <44780652+casmirconsensys@users.noreply.github.com>
2019-01-26 18:43:36 -0800

    Update SimpleBank.sol

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -1,70 +1,95 @@
-pragma solidity ^0.4.13;
-contract SimpleBank {
+pragma solidity ^0.5.0;
 
-    /* Fill in the keyword. Hint: We want to protect our users balance from other contracts*/
-    mapping (address => uint) balances;
-    
-    /* Fill in the keyword. We want to create a getter function and allow contracts to be able to see if a user is enrolled.  */
-    mapping (address => bool) enrolled;
+contract SupplyChain {
 
-    /* Let's make sure everyone knows who owns the bank. Use the appropriate keyword for this*/
-    address owner;
 
-    // Events - publicize actions to external listeners
-    
-    /* Add an argument for this event, an accountAddress */
-    event LogEnrolled();
+  address owner;
+  uint skuCount;
+  
+  mapping (uint => Item) public items;
 
-    /* Add 2 arguments for this event, an accountAddress and an amount */
-    event LogDepositMade();
-
-    /* Create an event called LogWithdrawal */
-    /* Add 3 arguments for this event, an accountAddress, withdrawAmount and a newBalance */
-
-    // Constructor, can receive one or many variables here; only one allowed
-    constructor() {
-        /* Set the owner to the creator of this contract */
-    }
-
-    /// @notice Enroll a customer with the bank
-    /// @return The users enrolled status
-    // Log the appropriate event
-    function enroll() public returns (bool){
-    }
-
-    /// @notice Deposit ether into bank
-    /// @return The balance of the user after the deposit is made
-    // Add the appropriate keyword so that this function can receive ether
-    function deposit() public returns (uint) {
-        /* Add the amount to the user's balance, call the event associated with a deposit,
-          then return the balance of the user */
-    }
-
-    /// @notice Withdraw ether from bank
-    /// @dev This does not return any excess ether sent to it
-    /// @param withdrawAmount amount you want to withdraw
-    /// @return The balance remaining for the user
-    function withdraw(uint withdrawAmount) public returns (uint) {
-        /* If the sender's balance is at least the amount they want to withdraw,
-           Subtract the amount from the sender's balance, and try to send that amount of ether
-           to the user attempting to withdraw. IF the send fails, add the amount back to the user's balance
-           return the user's balance.*/
-    }
-
-    /// @notice Get balance
-    /// @return The balance of the user
-    // A SPECIAL KEYWORD prevents function from editing state variables;
-    // allows function to run locally/off blockchain
-    function balance() public returns (uint) {
-        /* Get the balance of the sender of this transaction */
-    }
-
-    // Fallback function - Called if other functions don't match call or
-    // sent ether without data
-    // Typically, called when invalid data is sent
-    // Added so ether sent to this contract is reverted if the contract fails
-    // otherwise, the sender's money is transferred to contract
-    function() {
-        revert();
-    }
+  enum State{ForSale,Sold,Shipped,Received}
+  
+  struct Item  {
+    string name;
+    uint sku;
+    uint price; 
+    State state;
+    address seller;
+    address buyer;
 }
+  
+    event ForSale( uint sku);
+    event Sold(uint sku);
+    event Shipped(uint sku);
+    event Received( uint sku);
+
+
+  modifier onlyOwner () {require(msg.sender == owner); _;}
+  modifier verifyCaller (address _address) { require (msg.sender == _address); _;}
+  modifier paidEnough(uint _price) { require(msg.value >= _price); _;}
+  modifier checkValue(uint _sku) {
+    require(msg.value > items[_sku].price);
+    _;
+    uint _price = items[_sku].price;
+    uint amountToRefund = msg.value - _price;
+    items[_sku].buyer.transfer(amountToRefund);
+  }
+
+ 
+  modifier forSale(uint _sku) { require(items[_sku].state == State.ForSale); _; }
+  modifier sold(uint _sku) { require(items[_sku].state == State.Sold); _; }
+  modifier shipped(uint _sku) { require(items[_sku].state == State.Shipped); _;}
+  modifier received(uint _sku) {require(items[_sku].state == State.Shipped); _;}
+
+
+  constructor() public {
+    owner = msg.sender;
+    skuCount= 0;
+    
+  }
+
+  function addItem(string memory _name, uint _price) public returns(bool){
+    emit ForSale(skuCount);
+    items[skuCount] = Item({name: _name, sku: skuCount, price: _price, state: State.ForSale, seller: msg.sender, buyer: address(0)});
+    skuCount = skuCount + 1;
+    return true;
+  }
+
+  function buyItem(uint _sku)
+    public payable forSale(_sku) paidEnough(items[_sku].price) checkValue(_sku)
+  {
+      items [_sku].seller.transfer(items[_sku].price);
+      items[_sku].buyer = msg.sender;
+      items[_sku].state = State.Sold;
+      emit Sold(_sku);
+  }
+
+  
+  function shipItem(uint sku)
+    public sold(sku) verifyCaller(items[sku].seller) 
+  {
+      items[sku].state = State.Shipped;
+      emit Shipped(sku);
+      
+  }
+
+  function receiveItem(uint sku)
+    public shipped(sku) verifyCaller(items[sku].buyer)
+  {
+      items[sku].state = State.Recieved;
+      emit Received(sku);
+  }
+
+  function fetchItem(uint _sku) public view returns (string name, uint sku, uint price, uint state, address seller, address buyer) {
+    name = items[_sku].name;
+    sku = items[_sku].sku;
+    price = items[_sku].price;
+    state = uint(items[_sku].state);
+    seller = items[_sku].seller;
+    buyer = items[_sku].buyer;
+    return (name, sku, price, state, seller, buyer);
+  }
+
+}
+
